40f9029c4b2e4663640af7935df220a4015cbcd1
AndreHdSP221 <andrehsp22@gmail.com>
2025-05-25 21:55:59 -0300

    Added templates, static files and pages

--- projetodjango/settings.py
+++ projetodjango/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'recipes',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'projetodjango.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'base_templates', 
+        ], # Informar a pasta dos templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
---
+++ recipes/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+# Cliente (Request) -> Servidor (Response)
+# Cliente (Request) <- Servidor (Response)
+
+# HTTP REQUEST ou Render
+def home(request): # http pede envia uma request e ele retorna um http response ou render
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'André Henrique'
+    })
+
